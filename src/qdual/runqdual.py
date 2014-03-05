a082c2918eb57c0a54bf0073e1e00027400d34b0
Rephael Wenger <wenger@cse.ohio-state.edu>
2014-03-05 14:25:07 -0500

    runqdual.py runs qdual and then ijkmeshinfo. testqdual.py generates a random file and then runs ijkmeshinfo.

---
+++ src/qdual/runqdual.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+
+import os
+import sys
+
+offFile = 'out.off'
+
+if (len(sys.argv) <= 1) :
+    print 'Usage error.  Usage: ', sys.argv[0], " [qdual options] <isovalue> {nrrdFile}"
+    exit(10)
+
+qdual_command_line = ' '.join(sys.argv[1:])
+qdual_command_line = 'qdual -s -trimesh -move_vertex -o ' + offFile + ' ' + qdual_command_line
+
+print 'Executing: ', qdual_command_line
+
+os.system(qdual_command_line)
+
+ijkmeshinfo_command_line = 'ijkmeshinfo -manifold -terse out.off'
+os.system(ijkmeshinfo_command_line)
+
+ijkmeshinfo_command_line = 'ijkmeshinfo out.off'
+os.system(ijkmeshinfo_command_line)
+
+
+
+
